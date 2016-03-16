0f479affa0cc6ddbe0029408aea74919f1b6078a
Curtis Blackwell <blackwell.curtis@gmail.com>
2016-03-16 15:14:51 +0900

    :hatching_chick:

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <hex-dropdown label="Menu">
+      <ul>
+        <li><a href="#">Edit Profile</a></li>
+        <li><a href="#">Settings</a></li>
+      </ul>
+    </hex-dropdown>
+  </div>
+</template>
+
+<script>
+  import HexDropdown from './components/HexDropdown.vue'
+
+  export default {
+    components: {
+      HexDropdown
+    }
+  }
+</script>
---
+++ src/components/HexDropdown.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- Wrap in a div to prevent fragmentation. -->
+  <div>
+    <button @click="toggleDropdown">{{ label }}</button>
+    <div v-show="active" :transition="effect">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      // This shows/hides the dropdown's content.
+      active: Boolean,
+      effect: String,
+      // This serves as the button text.
+      label: {
+        type:     String,
+        required: true,
+      },
+    },
+
+    methods: {
+      /**
+       * Toggle the dropdown content's visibility.
+       *
+       * @author Curtis Blackwell
+       * @return {void}
+       */
+      toggleDropdown() {
+        this.active = !this.active;
+      }
+    }
+  }
+</script>
