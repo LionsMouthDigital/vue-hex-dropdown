8fc123399992dc8649faa057c36eb508dc6e0ff5
Curtis Blackwell <blackwell.curtis@gmail.com>
2016-06-29 12:53:45 -0500

    :bug: fix export

---
+++ src/components/_HexDropdown.vue
@@ -0,0 +1,11 @@
+<script>
+  import HexDropdown       from './HexDropdown.vue';
+  import HexDropdownPanel  from './HexDropdownPanel.vue';
+  import HexDropdownToggle from './HexDropdownToggle.vue';
+
+  export default {
+    HexDropdown,
+    HexDropdownPanel,
+    HexDropdownToggle,
+  }
+</script>
